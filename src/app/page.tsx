92224257145b76df087e35a1aaa64dd0c15df32c
Kyrylo Klymenko <klimenkomud@gmail.com>
2024-11-13 17:34:05 +0100

    Add user stats card and game guide

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-import { ActionDialog } from "@/components/game/ActionDialog";
-import { ColorModeButton } from "@/components/ui/color-mode";
 import { ConnectWalletButton } from "@/components/ConnectWalletButton";
-import { Box, Container, Flex, Heading } from "@chakra-ui/react";
+import { ActionDialog } from "@/components/game/ActionDialog";
+import { GameGuide } from "@/components/game/GameGuide";
+import { ColorModeButton } from "@/components/ui/color-mode";
+import { StatLabel, StatRoot, StatValueText } from "@/components/ui/stat";
+import {
+  Box,
+  Card,
+  Container,
+  Flex,
+  Heading,
+  HStack,
+  Icon,
+} from "@chakra-ui/react";
+import { GiHoneypot, GiMoneyStack, GiTrophy } from "react-icons/gi";
 
 export default function Home() {
   return (
@@ -22,10 +33,48 @@ export default function Home() {
         pt={8}
         height="calc(100vh - 72px)"
         display="flex"
-        flexDirection="column"
-        alignItems="center"
+        justifyContent="center"
+        gap={4}
+        flexWrap="wrap"
       >
         <ActionDialog />
+        <Flex flexDirection="column" gap={4} mt={4}>
+          <Card.Root>
+            <Card.Header>
+              <Card.Title>Your Stats</Card.Title>
+            </Card.Header>
+            <Card.Body gap={2} flexDirection={{ base: "column", md: "row" }} alignItems="center" justifyContent="center">
+              <StatRoot maxW="200px" borderWidth="1px" p="4" rounded="md">
+                <HStack justify="space-between">
+                  <StatLabel>Rank</StatLabel>
+                  <Icon color="fg.muted">
+                    <GiTrophy />
+                  </Icon>
+                </HStack>
+                <StatValueText>19</StatValueText>
+              </StatRoot>
+              <StatRoot maxW="200px" borderWidth="1px" p="4" rounded="md">
+                <HStack justify="space-between">
+                  <StatLabel>Balance</StatLabel>
+                  <Icon color="fg.muted">
+                    <GiMoneyStack />
+                  </Icon>
+                </HStack>
+                <StatValueText>32.12 SIP</StatValueText>
+              </StatRoot>
+              <StatRoot maxW="200px" borderWidth="1px" p="4" rounded="md">
+                <HStack justify="space-between">
+                  <StatLabel>Rewards</StatLabel>
+                  <Icon color="fg.muted">
+                    <GiHoneypot />
+                  </Icon>
+                </HStack>
+                <StatValueText>19.22 SIP</StatValueText>
+              </StatRoot>
+            </Card.Body>
+          </Card.Root>
+          <GameGuide />
+        </Flex>
       </Container>
     </>
   );
---
+++ src/components/game/GameGuide.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import {
+  TimelineConnector,
+  TimelineContent,
+  TimelineDescription,
+  TimelineItem,
+  TimelineRoot,
+  TimelineTitle,
+} from "@/components/ui/timeline";
+import { Box, Button, Card, Collapsible, Heading } from "@chakra-ui/react";
+import { useState } from "react";
+import { FaArrowDown } from "react-icons/fa";
+import { GiHoneypot, GiNoseFront, GiTrophy } from "react-icons/gi";
+import { IoPawSharp } from "react-icons/io5";
+
+export function GameGuide() {
+  const [collapsed, setCollapsed] = useState(true);
+  return (
+    <Card.Root>
+      <Collapsible.Root
+        open={!collapsed}
+        onOpenChange={() => setCollapsed((prevState) => !prevState)}
+      >
+        <Card.Header>
+          <Collapsible.Trigger asChild>
+            <Button variant="ghost" gap={2}>
+              <Heading>🐻🍯 BoopTheSnoot: A Beary Sweet Adventure!</Heading>
+              <Box
+                transform={collapsed ? "rotate(0deg)" : "rotate(180deg)"}
+                transition="transform 0.2s ease-in-out"
+              >
+                <FaArrowDown />
+              </Box>
+            </Button>
+          </Collapsible.Trigger>
+        </Card.Header>
+        <Card.Body>
+          <Collapsible.Content>
+            <TimelineRoot maxW="400px">
+              <TimelineItem>
+                <TimelineConnector>
+                  <IoPawSharp />
+                </TimelineConnector>
+                <TimelineContent>
+                  <TimelineTitle>Step 1: Stash Your $SIP</TimelineTitle>
+                  <TimelineDescription>
+                    {`Chuck your $SIP into the honey pot! More $SIP means you're a bigger, comfier bera in the Bera Cave!`}
+                  </TimelineDescription>
+                </TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineConnector>
+                  <GiHoneypot />
+                </TimelineConnector>
+                <TimelineContent>
+                  <TimelineTitle>
+                    Step 2: Claim Your Liquid Gold JUGs
+                  </TimelineTitle>
+                  <TimelineDescription>
+                    Every forest tick (block), your JUGs fill up with a mix of
+                    $SIP and $HONEY!
+                  </TimelineDescription>
+                </TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineConnector>
+                  <GiNoseFront />
+                </TimelineConnector>
+                <TimelineContent>
+                  <TimelineTitle>Step 3: Boop That Snoot!</TimelineTitle>
+                  <TimelineDescription>
+                    Decide to grow bigger or shrink down. Big bera or small
+                    bera? Lazy lounger or honey hustler? You decide!
+                  </TimelineDescription>
+                </TimelineContent>
+              </TimelineItem>
+              <TimelineItem>
+                <TimelineConnector>
+                  <GiTrophy />
+                </TimelineConnector>
+                <TimelineContent>
+                  <TimelineTitle>Step 4: Eye on the Honey Forest</TimelineTitle>
+                  <TimelineDescription>
+                    Check out other beras and their ranks. Can you outmaneuver
+                    them for more honey?
+                  </TimelineDescription>
+                </TimelineContent>
+              </TimelineItem>
+            </TimelineRoot>
+          </Collapsible.Content>
+        </Card.Body>
+      </Collapsible.Root>
+    </Card.Root>
+  );
+}
